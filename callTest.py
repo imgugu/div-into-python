a7fee816b735cce9b9850831d5a48681de1469bf
jlgu <xqg8484@163.com>
2013-09-12 09:52:12 +0800

    test

---
+++ callTest.py
@@ -0,0 +1,10 @@
+#测试__call__方法
+class CallTst():
+    def __init__(self):
+        print "i am init fun"
+
+    def __call__(self, x, y):
+        print "i am call fun "+x+y
+
+x = CallTst()
+x("2","3")
